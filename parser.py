f65ccef00c5172e789fb7daaec92b1893c613f2d
Ilya Fukin <fukin.ii@mipt.ru>
2024-05-10 17:21:42 +0300

    Доработка парсера

--- parser.py
+++ parser.py
@@ -1,18 +1,90 @@
+import re
+
 from bs4 import BeautifulSoup
 import requests
 
-word = "wireless"
-url = f"https://www.multitran.com/m.exe?l1=1&l2=2&s={word}"
-response = requests.get(url)
-html_content = response.text
 
-# Создание объекта Beautiful Soup
-soup = BeautifulSoup(html_content, 'html.parser')
+def get_translation(word):
+    # word = "wireless"
+    url = f"https://www.multitran.com/m.exe?l1=1&l2=2&s={word}"
+    response = requests.get(url)
+    html_content = response.text
 
-# Поиск элемента <body> с определенным стилем
-body = soup.body
-container = body.find('div', class_='container')
-mclass_elements = container.find_all(class_='mclass160_10')[2]
-first_table = mclass_elements.find('table')
+    # Создание объекта Beautiful Soup
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-print(first_table)
+    # Поиск элемента <body> с определенным стилем
+    body = soup.body
+    container = body.find('div', class_='container')
+    mclass_elements = container.find_all(class_='mclass160_10')[2]
+    first_table = mclass_elements.find('table')
+    rows = first_table.find_all('tr')
+
+    flag = 0
+
+    all_trans = []
+
+    for row in rows:
+
+        # Находим все элементы <td> в текущей строке
+        cells = row.find_all('td')
+        # Если в строке есть два элемента <td>, то это наши данные subj и trans
+        # if len(cells) == 2:
+        # print(len(cells))
+        subj = cells[0].text.strip()
+        if word in subj:
+            flag = 1
+            # print("aaaaaaaa")
+        else:
+            if len(cells) > 1 and flag > 0:
+                trans = cells[1].text.strip()
+                trans = trans.split(';')
+                trans = [re.sub(r'\s*\([^)]*\)', '', el) for el in trans]
+                if len(trans) > 4:
+                    trans = trans[:4]
+                if len(all_trans) > 10:
+                    trans = [trans[0]]
+                # print("subj:", subj)
+                # print("trans:", trans)
+                all_trans.append(trans)
+                # print("---------------------")
+                flag -= 1
+
+    all_trans = [el for sub in all_trans for el in sub]
+
+    return all_trans
+    # print(all_trans)
+
+
+if __name__ == "__main__":
+    # Открываем файл для чтения
+    all_res = []
+    with open("your_file.txt", "r") as file:
+        # Читаем все содержимое файла
+        words = file.read().split()
+
+        # Перебираем слова и вызываем для каждого get_translation
+        for word in words:
+            result = get_translation(word)
+            all_res.append([word, result])
+            print(result)
+
+    with open('dictionary.txt', 'w') as file:
+        for res in all_res:
+            file.write(res[0] + ": " + ', '.join(res[1]) + '.\n')
+
+# if __name__ == "__main__":
+#     word = "wireless"
+#     result = get_translation(word)
+#     print(result)
+
+# for tr_i in tr:
+#     td = tr_i.find_all('td')
+#     if len(td) == 2:
+#         print("subj:")
+#         print(td[0].find("a"))
+# td1 = tr_i.find('td')
+# print("=======\n", len(td))
+# print(td1)
+
+# print(tr0)
