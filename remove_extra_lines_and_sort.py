598e4cd448f022fa8ca615afe91b12d3565e6a30
Ilya Fukin <fukin.ii@mipt.ru>
2024-05-12 13:46:50 +0300

    Добавил сортировку словаря

---
+++ remove_extra_lines_and_sort.py
@@ -0,0 +1,17 @@
+def remove_extra_newlines_and_sort(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as input_file:
+        lines = input_file.readlines()
+
+    words = [word.strip() for word in lines if word.strip()]
+
+    sorted_words = sorted(words)
+
+    with open(output_file_path, 'w') as output_file:
+        for word in sorted_words:
+            output_file.write(word + '\n')
+
+
+if __name__ == "__main__":
+    input_file_path = 'words_thinned.txt'
+    output_file_path = 'words_thinned_wo_lines.txt'
+    remove_extra_newlines_and_sort(input_file_path, output_file_path)
